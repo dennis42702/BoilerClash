7659fd5e1e381405c3cca5b24df291a589675db6
dennis42702 <dennis42702@gmail.com>
2025-02-22 03:46:37 -0500

    edited model attributes

--- backend/models/Building.js
+++ backend/models/Building.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const BuildingSchema = new mongoose.Schema({
     name: {type:String, required:true},
+    //coord 
     conqueredByUser: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, 
     conqueredByTeam: {type:String, required:true, default: null},
     total_people: {type:Number, default: 0}, //query sessions every like few minutes?
