b26a8f722aa3d4a51a3e595a2e5f72db60f69ff6
Paaaark <hifipark0801@gmail.com>
2025-02-22 03:09:54 -0500

    Navigation

--- frontend/components/HomeScreen.js
+++ frontend/components/HomeScreen.js
@@ -1,14 +1,13 @@
 // HomeScreen.js
 import * as React from "react";
 import { BottomNavigation, Text } from "react-native-paper";
+import LeaderboardFragment from "./fragments/leaderboard_june";
 
-const MusicRoute = () => <Text>Music</Text>;
+const MusicRoute = () => <LeaderboardFragment />;
 
 const AlbumsRoute = () => <Text>Albums</Text>;
 
 const RecentsRoute = () => <Text>Recents</Text>;
-
-const NotificationsRoute = () => <Text>Notifications</Text>;
 
 const HomeScreen = () => {
   const [index, setIndex] = React.useState(0);
@@ -21,19 +20,12 @@ const HomeScreen = () => {
     },
     { key: "albums", title: "Albums", focusedIcon: "album" },
     { key: "recents", title: "Recents", focusedIcon: "history" },
-    {
-      key: "notifications",
-      title: "Notifications",
-      focusedIcon: "bell",
-      unfocusedIcon: "bell-outline",
-    },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     music: MusicRoute,
     albums: AlbumsRoute,
     recents: RecentsRoute,
-    notifications: NotificationsRoute,
   });
 
   return (
