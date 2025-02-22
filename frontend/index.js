fd6286c88e89cbba0d1a27cfd19d059391a9a9f1
Paaaark <hifipark0801@gmail.com>
2025-02-22 03:02:58 -0500

    Navigation

--- frontend/index.js
+++ frontend/index.js
@@ -2,15 +2,24 @@
 import React from "react";
 import { AppRegistry } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
-import HomeScreen from "./components/HomeScreen"; // Ensure this path is correct
-import { name as appName } from "./app.json";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/screens/login";
 import ForgotPasswordScreen from "./components/screens/forgotPW";
-import SignUpStep1 from "./components/screens/signup_id";
-import SignUpStep2 from "./components/screens/signup_info";
+
+const Stack = createNativeStackNavigator();
+
 const App = () => (
   <PaperProvider>
-    <SignUpStep2 />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   </PaperProvider>
 );
 
